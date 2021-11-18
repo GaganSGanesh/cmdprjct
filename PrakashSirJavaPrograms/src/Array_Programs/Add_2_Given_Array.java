6479d4b1f874ea4d76908cdfdd5db0dcdf54afe2
Milu <samalbiswajit51@gmail.com>
2021-11-18 23:15:16 +0530

    RA

--- PrakashSirJavaPrograms/src/Array_Programs/Add_2_Given_Array.java
+++ PrakashSirJavaPrograms/src/Array_Programs/Add_2_Given_Array.java
@@ -3,23 +3,51 @@ package Array_Programs;
 public class Add_2_Given_Array {
 
 	public static void main(String[] args) {
-		int[] a= {30,10,40,60,20,50};
+		int[] a= {30,10,40,60,20};
 		int[] b= {30,10,40,60,20,50,70,80};
-		int gl=a.length>b.length?a.length:b.length;
-		int sl=a.length<b.length?a.length:b.length;
-		int[] c=new int[gl];
-		
-		
-		
-		for(int k=0;k<gl;k++) {
-			if(k<=sl)
-				c[k]=a[k]+b[k];
-			else
-				
-			
+		if(a.length>b.length) {
+			int[] c=new int[a.length];
+			for(int i=0;i<b.length;i++) {
+				c[i]=b[i];		
+			}			
+			for(int i=0;i<c.length;i++) {
+				System.out.println( c[i]+a[i]);	
+			}			
 		}
-
+		else {
+			int[] c=new int[b.length];
+			for(int i=0;i<a.length;i++) {
+				c[i]=a[i];				
+			}
+			for(int i=0;i<c.length;i++) {
+				System.out.println( c[i]+b[i]);	
+			}
+		}
+	
 	}
 
 }
 
+class Add_2_Given_Array2 {
+
+	public static void main(String[] args) {
+		int[] a= {30,10,40,60,20};
+		int[] b= {30,10,40,60,20,50,70,80};
+		if(a.length>b.length) {
+			for(int i=0;i<a.length;i++) {
+				if(i<b.length)
+				System.out.println( a[i]+b[i]);	
+				else		
+					System.out.println(a[i]);	
+			}							
+		}
+		else {
+			for(int i=0;i<b.length;i++) {
+				if(i<a.length)
+				System.out.println( a[i]+b[i]);	
+				else		
+					System.out.println(b[i]);	
+			}			
+	}
+ }
+}
